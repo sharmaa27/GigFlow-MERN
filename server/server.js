5d786bb86b22942bc113277c0d03a46e86b6f880
vipul sharma <vipulsharma@vipuls-MacBook-Air.local>
2026-01-13 01:13:51 +0530

    Fix: Use regex for catch-all route to prevent crash

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ const PORT = process.env.PORT || 4000;
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
 
-  app.get('*', (req, res) => {
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
   });
 }
